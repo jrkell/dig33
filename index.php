dd9b9a6feb6ee3c29378d08ab133a7eae8eac783
amaara <51810044+amaara@users.noreply.github.com>
2019-06-14 14:00:54 +1000

    Barb set up Gith

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
+<html>
+<head>
+  <title>Porque no los dos?</title>
+  <link rel="stylesheet" type="text/css" href="styles/styles.css">
+</head>
+<body>
 <h1>DIG33 Website Page</h1>
 <p>Beer and tequila.. together at last!</p>
 
 <?php
   echo "testing the cron job~~";
 ?>
+<h2>Barb is testing!</h2>
+</body>
