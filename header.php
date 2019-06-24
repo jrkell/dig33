8fcd35ccf298a546e4dcac3cbc1b62322f485ab0
jaredrkelly <jarredkelly@gmail.com>
2019-06-24 11:32:10 +1000

    created header, footer files and template for content

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<!-- This website by The Tequila Techies, for Curtin University DIG33-->
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <!-- reference to external font, css and js -->
+        <link rel="stylesheet" type="text/css" href="styles/style.css">
+        <link rel="stylesheet" type="text/css" href="styles/main.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script src="scripts/scripts.js"></script>
+        <title><?php echo $title; ?></title>
+</head>
--- template.php
+++ template.php
@@ -1,27 +1,19 @@
+<!-- Header -->
 <?php
   $title = "DESPERADOS - Porque no los dos?";
   include 'header.php';
 ?>
 
+
 <!-- PAGE CONTENT START -->
 <body>
   
-  
-  <h1>DIG33 Website Page</h1>
-  <p>Beer and tequila.. together at last!</p>
-
-  <?php
-    echo "testing the cron job~~";
-  ?>
-  <h2>Barb is testing!</h2>
-      <h2>Tania is Testing</h2>
-      <h2>Dion's Test Update</h2>
-
 
 </body>
 <!-- PAGE CONTENT END -->
 
 
+<!-- Footer -->
 <?php
   $title = "DESPERADOS - Porque no los dos?";
   include 'footer.php';
