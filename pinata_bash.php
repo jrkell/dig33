10f0df19d6271b6bcd67da5395e421491365997d
nismotaur <31636049+nismotaur@users.noreply.github.com>
2019-07-20 00:46:34 +1000

    Semi-functional login/signup code

--- pinata_bash.php
+++ pinata_bash.php
@@ -7,10 +7,29 @@
         <div id="pinata-container">
             <!--Checks if user has logged in or not-->
             <?php
-            if(isset($_COOKIE["user_verified"])) {
+            if(isset($_COOKIE["user_verified"]))
+            {
                 include "pinata.php";
-            } else {
+            }
+            else
+            {
+                if(isset($_GET['fail']))
+                {
+                    if($_GET['fail'] == 'pass')
+                    {
+                        echo "<p class='error'>Incorrect Password!</p>";
+                    }
+                    else if($_GET['fail'] == 'user')
+                    {
+                        echo "<p class='error'>No account associated with the email address entered!</p>";
+                    }
+                }
+                else
+                {
+                    echo "<p class='error'>Cookie not set!</p>";
+                }
                 include "user_login.php";
+                
             }
             ?>
         </div>
