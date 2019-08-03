9375426f29d8c3599262132f5fbb9bfcf44dcf08
amaara <51810044+amaara@users.noreply.github.com>
2019-08-03 13:55:22 +1000

    Create 404.php

---
+++ a3/404.php
@@ -0,0 +1,28 @@
+<!-- Header -->
+<?php
+  $title = "DESPERADOS - Porque no los dos?";
+  include 'header.php';
+?>
+
+<!-- MAIN CONTENT -->
+<main id="container">
+  <div class="container-fluid">
+      <div class="text-center">
+          <h1>OOPS! That page is missing?</h1>
+          <p>Sorry, we can't find that page. It might be an old link or maybe it moved.</p>
+      </div>
+
+  </div>
+
+
+
+</main>
+</body>
+<!-- PAGE CONTENT END -->
+
+
+<!-- Footer -->
+<?php
+  $title = "DESPERADOS - Porque no los dos?";
+  include 'footer.php';
+?>
