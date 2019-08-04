ec3f4d619040782de1859f7d3dac127f608d69c9
amaara <51810044+amaara@users.noreply.github.com>
2019-08-05 00:21:34 +1000

    404 update

--- a3/404.php
+++ a3/404.php
@@ -6,11 +6,11 @@
 
 <!-- MAIN CONTENT -->
 <main id="container">
-  <div class="container-fluid">
-      <div class="text-center">
-          <img src="images/404-bg.png" class="img-fluid" />
-          <h1>OOPS! That page is missing.</h1>
+  <div class="container-fluid bg-404">
+      <div class="text-center" style="padding-top:500px">
+          <h1>That page is missing.</h1>
           <p>Sorry, we can't find that page. It might be an old link or maybe it moved.</p>
+          <p>
       </div>
 
   </div>
