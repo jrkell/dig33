44848727f9a02ed3fc1367abe95840f24cfd2ad4
amaara <51810044+amaara@users.noreply.github.com>
2019-08-05 12:32:50 +1000

    Event page update

--- a3/events.php
+++ a3/events.php
@@ -12,48 +12,49 @@
       <h1 class="text-center">Aussie Music Festivals</h1>
     </div>
   </div>
-
-<div class="row">
-  <div class="col-sm-6">
+</div>
+<div class="row no-gutters">
+  <div class="col-sm-6 event-text text-center">
+    <img src="images/icon-orange.png" class="tt-icon" alt="Icon" />
     <h2>JAZZ CLUB</h2>
       <p>Get your groove on at the big band music festival. <p>
       <p>Artists playing tribute to some of the worlds most famous Jazz musicians in an open-air concert that will get your toes tapping and your fingers clicking.</p>
       <p>Jazz is making its way back to the top of the pack and you should not miss it.</p>
   </div>
   <div class="col-sm-6">
-      <img src="images/events/event-jazz.png" class="img-fluid" alt="Image Placeholder"/>
+      <img src="images/events/event-jazz.jpg" class="img-fluid" alt="Jazz Club"/>
   </div>
 </div>
 
-<div class="row">
-  <div class="col-sm-6 text-right">
-      <img src="images/events/event-rock.png" class="img-fluid" alt="Image Placeholder"/>
-  </div>
+<div class="row no-gutters">
   <div class="col-sm-6">
+      <img src="images/events/event-rock.jpg" class="img-fluid" alt="River Rock Fest"/>
+  </div>
+  <div class="col-sm-6 event-text text-center">
+    <img src="images/icon-green.png" class="tt-icon" alt="Icon"/>
     <h2>RIVER ROCK FEST</h2>
     <p>Aussie legends will be playing in a town near you for the River Rock Fest.</p>
     <p>With big names like Jimmy Barnes and the Chantoozies you can’t miss your chance to step back in time and sing along to the classic hits, some music never goes out of style,
   it just gains a bigger audience.</p>
     <p>An open air event with drinks available at the venue.</p>
   </div>
-
 </div>
 
-<div class="row">
-
-  <div class="col-sm-6">
+<div class="row no-gutters">
+  <div class="col-sm-6 event-text text-center">
+    <img src="images/icon-red.png" class="tt-icon" alt="Icon" />
     <h2>BIG DAY OUT</h2>
       <p>Top bands from Australia and over seas will come together to present an awesome day and evening of top hits.</p>
       <p>Big Day Out has been in hibernation but is ready to explode back into action this year for a one-day event.</p>
       <p>Be the one who attended the biggest event of the year, get in early and get your tickets.</p>
   </div>
   <div class="col-sm-6">
-      <img src="images/events/event-bdo.png" class="img-fluid" alt="Image Placeholder"/>
+      <img src="images/events/event-bdo.jpg" class="img-fluid" alt="Big Day Out"/>
   </div>
 </div>
 
 
-</div> <!-- close container-fluid -->
+<!-- close container-fluid -->
 </main>
 
 <!-- PAGE CONTENT END -->
