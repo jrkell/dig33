8de09833da3725471e5ffa542bb863308885378f
amaara <51810044+amaara@users.noreply.github.com>
2019-08-06 10:20:08 +1000

    event page update

--- a3/about.php
+++ a3/about.php
@@ -14,25 +14,23 @@
 </div>
   <div class="row no-gutters">
     <div class="col-sm-6 text-center">
-      <img src="images/about_image.png" class="img-fluid" alt="Desperados Australia"/>
+      <img src="images/australia.jpg" class="img-fluid" alt="Desperados Australia"/>
 
     </div>
-    <div class="col-sm-6">
-      <p>We have been wanting to come to Australia for quite some time, and now we are here.
-        With the Australian way of life our product is a natural introduction to the products currently available on your stockists’ shelves.</p>
+    <div class="col-sm-6 event-box text-center">
+      <div class="event-text">
+          <img src="images/icon-yellow.png" class="tt-icon" alt="Icon"/>
+      <p>We have been wanting to come to Australia for quite some time, and now we are here.</p>
       <p>We offer you a new unique experience with our beer+ product.
           Desperados wants to be there enhancing your party spirit, bringing the spice to your life with a daring edge found in the touch of tequila.</p>
 
       <h2>Beer +</h2>
 
       <p>The beer+ industry is growing, and we are proud to be a leader of that industry with the world’s first tequila flavoured beer.</p>
-      <p>Our product is made to a high quality with only the best ingredients. High quality tequila is our specialty and by adding it to our beer we have
+      <p>Flavour is our specialty and by adding spices and zest to our beer we have
         created something that you have to taste to believe.</p>
+      </div>
 
-      <h2>Competitions</h2>
-      <p>To coincide with our Australian release, we have a competition that is open only to Australian residents.</p>
-      <p>The Pinata Bash will be a limited time only competition for our launch. But watch this space for upcoming competitions.
-        We have random competitions for all Desperados customers to participate in that are fun to play and have great prizes.</p>
       </div>
     </div>
 
@@ -40,6 +38,9 @@
       <div class="col-sm-6 event-box text-center">
         <div class="event-text">
           <img src="images/icon-green.png" class="tt-icon" alt="Icon"/>
+          <h2>Promotions</h2>
+          <p>To coincide with our Australian release, we have a competition that is open to Australian residents.</p>
+          <p>The Pinata Bash is a limited time only competition for our launch. But watch this space for upcoming competitions for all Desperados customers.</p>
         <h2>Save the Koala</h2>
         <p>To celebrate our launch we will be donating $2 from every bottle of Desperados Original to the Save the Koala Fund.</p>
       </div>
