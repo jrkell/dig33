d10eaa9aa3f5d8c1eec948404ccde3cd72b0acc3
amaara <51810044+amaara@users.noreply.github.com>
2019-08-08 14:04:41 +1000

    social media links in footer

--- a3/footer.php
+++ a3/footer.php
@@ -3,9 +3,9 @@
     <script src="js/scripts.js"></script>
 
     <div class="social">
-      <img src="images/social/facebook.png" alt="Facebook link" />
-      <img src="images/social/twitter.png" alt="Twitter link" />
-      <img src="images/social/instagram.png" alt="Instagram link" />
+      <a href="https://www.facebook.com/desperados" target="blank"><img src="images/social/facebook.png" alt="Facebook link" /></a>
+      <a href="https://www.instagram.com/desperados/" target="blank"><img src="images/social/instagram.png" alt="Instagram link" /></a>
+      <a href="https://www.youtube.com/user/desperados" target="blank"><img src="images/social/youtube.png" alt="YouTube link" />
     </div>
     <div class="disclaimer">This website has been created as part of an assignment in an approved course of study for Curtin University and contains copyright material not created by the author. All copyright material used remains copyright of the respective owners and has been used here pursuant to Section 40 of the Copyright Act 1968 (Commonwealth of Australia). No part of this work may be reproduced without consent of the original copyright owners. See code comments for references.</div>
 </footer>
