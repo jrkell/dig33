6d0590a773b432350582ccceef2f45e5ffdc9fca
amaara <51810044+amaara@users.noreply.github.com>
2019-08-08 13:26:47 +1000

    updating layouts

--- a3/footer.php
+++ a3/footer.php
@@ -1,6 +1,12 @@
 <footer>
     <!--    Link to external JS for elements which require it in the footer rather than in the header-->
     <script src="js/scripts.js"></script>
+
+    <div class="social">
+      <img src="images/social/facebook.png" alt="Facebook link" />
+      <img src="images/social/twitter.png" alt="Twitter link" />
+      <img src="images/social/instagram.png" alt="Instagram link" />
+    </div>
     <div class="disclaimer">This website has been created as part of an assignment in an approved course of study for Curtin University and contains copyright material not created by the author. All copyright material used remains copyright of the respective owners and has been used here pursuant to Section 40 of the Copyright Act 1968 (Commonwealth of Australia). No part of this work may be reproduced without consent of the original copyright owners. See code comments for references.</div>
 </footer>
 </body>
