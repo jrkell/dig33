1ae0776ae3356b8a81870d01626c555147d5a6f9
amaara <51810044+amaara@users.noreply.github.com>
2019-08-09 13:26:31 +1000

    Create store.php

---
+++ store.php
@@ -0,0 +1,32 @@
+<!-- Header -->
+<?php
+  $title = "WHERE TO BUY DESPERADOS - Porque no los dos?";
+  include 'header.php';
+?>
+
+<!-- MAIN CONTENT -->
+<main id="container">
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <h1>WHOLESALE STORE</h1>
+                <p>Thank you for placing your order</p>
+            </div>
+        </div>
+
+        <div class="row">
+
+      <div class="col-sm-4">
+
+      SHOPPING CART 
+      </div>
+</main>
+<!-- PAGE CONTENT END -->
+
+
+<!-- Footer -->
+<?php
+  $title = "DESPERADOS - Porque no los dos?";
+  include 'footer.php';
+?>
