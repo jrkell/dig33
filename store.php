d1a801a366f603e3e806c8279ee5de0c957e0725
amaara <51810044+amaara@users.noreply.github.com>
2019-08-10 12:48:07 +1000

    Update store.php

--- store.php
+++ store.php
@@ -10,16 +10,29 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-12 text-center">
-                <h1>WHOLESALE STORE</h1>
+                <h1>WHOLESALE ORDERS</h1>
                 <p>Thank you for placing your order</p>
             </div>
         </div>
 
+        <!-- php IF magic -->
         <div class="row">
 
-      <div class="col-sm-4">
+          <!-- php WHILE magic -->
+          <div class="col-sm-4">
+          <div class="card text-center">
+            <img class="card-img-top" src=" ... " alt="Product image">
+            <div class="card-body">
+              <p class="card-text">PRODUCT DESCRIPTION</p>
 
-      SHOPPING CART 
+              <!-- QUANTITY -->
+
+              <!-- ADD TO CART BUTTON -->
+
+
+            </div>
+          </div>
+        </div>
       </div>
 </main>
 <!-- PAGE CONTENT END -->
