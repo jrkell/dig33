e2213b444a0784e247932ad2e4de3cf8c5daec2d
amaara <51810044+amaara@users.noreply.github.com>
2019-08-11 10:54:42 +1000

    footer update and icon update

--- footer.php
+++ footer.php
@@ -1,14 +1,31 @@
-<footer>
+<footer class="footer-bg container-fluid">
+
     <!--    Link to external JS for elements which require it in the footer rather than in the header-->
     <script src="js/scripts.js"></script>
-    
-    <div class="social">
-      <a href="https://www.facebook.com/desperados" target="blank"><img src="images/social/facebook.png" alt="Facebook link" /></a>
-      <a href="https://www.instagram.com/desperados/" target="blank"><img src="images/social/instagram.png" alt="Instagram link" /></a>
-      <a href="https://www.youtube.com/user/desperados" target="blank"><img src="images/social/youtube.png" alt="YouTube link" />
-    </div>
 
-    <div class="disclaimer">This website has been created as part of an assignment in an approved course of study for Curtin University and contains copyright material not created by the author. All copyright material used remains copyright of the respective owners and has been used here pursuant to Section 40 of the Copyright Act 1968 (Commonwealth of Australia). No part of this work may be reproduced without consent of the original copyright owners. See code comments for references.</div>
+  <div class="row textured">
+    <div class="col-sm-6 disclaimer">
+      <p>Copyright &copy; 2019 Tequila Techies</p>
+    </div>
+    <div class="col-sm-6 social">
+      <p>Connect with us: </p>
+      <a href="https://www.youtube.com/user/desperados" target="blank">
+        <i class="fab fa-youtube"></i>
+      </a>
+      <a href="https://www.facebook.com/desperados" target="blank">
+        <i class="fab fa-facebook"></i>
+      </a>
+      <a href="https://www.instagram.com/desperados/" target="blank">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="https://twitter.com/Heineken" target="blank">
+        <i class="fab fa-twitter"></i>
+      </a>
+      </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-12 disclaimer">This website has been created as part of an assignment in an approved course of study for Curtin University and contains copyright material not created by the author. All copyright material used remains copyright of the respective owners and has been used here pursuant to Section 40 of the Copyright Act 1968 (Commonwealth of Australia). No part of this work may be reproduced without consent of the original copyright owners. See code comments for references.</div>
+  </div>
 </footer>
 </body>
 
