636038a2d8495396709d9de44a2df0c4468620de
amaara <51810044+amaara@users.noreply.github.com>
2019-08-11 23:04:29 +1000

    yes more nitpicking

--- footer.php
+++ footer.php
@@ -25,7 +25,14 @@
       </div>
   </div>
   <div class="row">
-    <div class="col-lg-12 disclaimer">This website has been created as part of an assignment in an approved course of study for Curtin University and contains copyright material not created by the author. All copyright material used remains copyright of the respective owners and has been used here pursuant to Section 40 of the Copyright Act 1968 (Commonwealth of Australia). No part of this work may be reproduced without consent of the original copyright owners. See code comments for references.</div>
+    <hr class="gold">
+    <div class="col-lg-12 disclaimer">
+      <hr class="gold">
+      <p>This website has been created as part of an assignment in an approved course of study for Curtin University and contains copyright material not created by the author.
+        All copyright material used remains copyright of the respective owners and has been used here pursuant to Section 40 of the Copyright Act 1968 (Commonwealth of Australia).
+        No part of this work may be reproduced without consent of the original copyright owners. See code comments for references.
+      </p>
+    </div>
   </div>
 </footer>
 </body>
