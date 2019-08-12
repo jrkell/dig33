4aa22a302a7c063858d5809ae7027a2424779519
amaara <51810044+amaara@users.noreply.github.com>
2019-08-12 16:32:53 +1000

    Update header.php

--- header.php
+++ header.php
@@ -78,7 +78,7 @@
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                           Stockists
                         </a>
-                        <div class="dropdown-menu show-bg" aria-labelledby="navbarDropdown">
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                           <a class="dropdown-item" href="stockists.php">FIND A STOCKIST</a>
                           <a class="dropdown-item" href="wholesale.php">WHOLESALE LOGIN</a>
                         </div>
