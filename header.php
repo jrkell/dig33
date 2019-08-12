dfd0c23ac6bb1bcfb975dcaac97f1dc1d46dbaed
amaara <51810044+amaara@users.noreply.github.com>
2019-08-12 16:24:12 +1000

    FOUND THE #$%@^ CSS SETTINGS FOR BOOTSTRAP DROPDOWN NAV

--- header.php
+++ header.php
@@ -16,7 +16,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
     <!-- reference to external font, css and js -->
     <link rel="stylesheet" type="text/css" href="styles/normalize.min.css">
     <link rel="stylesheet" type="text/css" href="styles/style.css">
@@ -24,10 +23,10 @@
     <link rel="stylesheet" type="text/css" href="styles/pinata.css">
     <!-- link to Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Arbutus%7CRanchers%7CAllerta&display=swap" rel="stylesheet">
-    <!-- Bootstrap 4 CDN -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <!-- Link to Font Awesome icons - https://www.w3schools.com/bootstrap4/bootstrap_icons.asp -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <!-- Bootstrap 4 CDN -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
@@ -56,7 +55,8 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
                         <a class="nav-link" href="index.php">Home</a>
@@ -71,23 +71,23 @@
                     <li class="nav-item">
                         <a class="nav-link" href="about.php">About</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="contact.php">Contact Us</a>
-                    </li>
+
 
                     <!-- Dropdown -->
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                           Stockists
                         </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <div class="dropdown-menu show-bg" aria-labelledby="navbarDropdown">
                           <a class="dropdown-item" href="stockists.php">FIND A STOCKIST</a>
-
-                          <div class="dropdown-divider"></div>
-                          <a class="dropdown-item" href="wholesale.php">WHOELSALE LOGIN</a>
+                          <a class="dropdown-item" href="wholesale.php">WHOLESALE LOGIN</a>
                         </div>
                       </li>
+
+                      <li class="nav-item">
+                          <a class="nav-link" href="contact.php">Contact Us</a>
+                      </li>
                 </ul>
-</div>
+              </div>
         </nav>
     </header>
