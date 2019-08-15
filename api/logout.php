be49dce45a56fae213c8e2042df7d5b634d7b13d
nismotaur <31636049+nismotaur@users.noreply.github.com>
2019-08-16 05:42:34 +1000

    pinata bash code entry

--- api/logout.php
+++ api/logout.php
@@ -3,14 +3,18 @@
     if(isset($_GET['stockist'])) {
         if(isset($_COOKIE['stockist_verified'])) {
         // set cookie to time in the past - browser deletes it
-            setcookie('stockist_verified', 'false', 0 , '/');
+            setcookie('stockist_verified', '', -1 , '/');
         }
     }
 
     if(isset($_GET['user'])) {
         if(isset($_COOKIE['user_verified'])) {
         // set cookie to time in the past - browser deletes it
-            setcookie('user_verified', 'false', 0 , '/');
+            setcookie('user_verified', '', -1 , '/');
+        }
+        if(isset($_COOKIE['code_entered'])) {
+            // set cookie to time in the past - browser deletes it
+            setcookie('code_entered', '', -1 , '/');
         }
     }
 
