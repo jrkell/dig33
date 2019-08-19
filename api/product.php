3c32a6b33510636583d2f1951b04549894d69b78
amaara <51810044+amaara@users.noreply.github.com>
2019-08-19 11:08:36 +1000

    team page update

--- api/product.php
+++ api/product.php
@@ -20,43 +20,45 @@ $product = mysqli_fetch_assoc(getProduct($title));
               <div class='boxed'>
                 <img src={$product['url']} class='img-fluid' alt={$product['title']} />
               </div>
-            </div>
-            <div class='col-lg-6 text-center'>
+          </div>
+          <div class='col-lg-6 text-center'>
               <div class='box-text'>
-              <img src='images/icon-green.png' class='tt-icon' alt='Tequila Icon' />
-              <h2 class='text-center'>{$product['title']}</h2>
-              <h3 class='text-center'>{$product['short_desc']}</h3><br>
-              <p>{$product['long_desc']}</p>
+                <img src='images/icon-green.png' class='tt-icon' alt='Tequila Icon' />
+                <h2 class='text-center'>{$product['title']}</h2>
+                <h3 class='text-center'>{$product['short_desc']}</h3><br>
+                <p>{$product['long_desc']}</p>
               </div>
-            </div>
-            </div>
+          </div>
+          </div> <!-- Close first row -->
+
         <div class='row' style='padding-top:40px;'>
           <div class='col-lg-6 text-center'>
             <div class='box-text'>
-            <img src='images/icon-orange.png' class='tt-icon' alt='Tequila Icon' />
-            <h2 class='text-center'>THE BEER+ EXPERIENCE</h2>
-              <div class='embed-responsive embed-responsive-16by9'>
-              <iframe class='embed-responsive-item' src='{$product['video']}' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>
-              </div>
+              <img src='images/icon-orange.png' class='tt-icon' alt='Tequila Icon' />
+              <h2 class='text-center'>THE BEER+ EXPERIENCE</h2>
+                <div class='embed-responsive embed-responsive-16by9'>
+                  <iframe class='embed-responsive-item' src='{$product['video']}' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>
+                </div>
             </div>
           </div>
+
           <div class='col-lg-6'>
-          <div class='table-responsive'>
-          <table class='table table-dark table-striped table-sm'>
-          <thead>
-          <tr>
-          <h2 class='text-center'>PRODUCT INFO</h2>
-            </tr>
+            <div class='table-responsive'>
+            <table class='table table-dark table-striped table-sm'>
+            <thead>
+              <tr>
+                <h2 class='text-center'>PRODUCT INFO</h2>
+              </tr>
             </thead>
             <tbody>
-            <tr>
-            <td class='w-30'>Contents</td><td class='w-70'> {$product['contents']}</td>
-            <tr>
-            <td>Ingredients</td><td> {$product['ingredients']}</td>
-            </tr>
-            <tr>
-            <td>Allergy information</td><td> {$product['allergens']}</td>
-            </tr>
+              <tr>
+                <td class='w-30'>Contents</td><td class='w-70'> {$product['contents']}</td>
+              <tr>
+                <td>Ingredients</td><td> {$product['ingredients']}</td>
+              </tr>
+              <tr>
+                <td>Allergy information</td><td> {$product['allergens']}</td>
+              </tr>
             <tr>
             <td>Alcohol percentage</td><td> " . number_format($product['alcohol'], 1) . "%</td>
             </tr>
--- team.php
+++ team.php
@@ -13,63 +13,62 @@
           <h2>Meet the Team</h2>
         </div>
       </div>
-    <div class="row box-blue">
-        <div class="col-lg-4 text-center">
-          <div class="card bg-dark text-white" style="width: 300px;">
-            <img class="card-img-top" src="images/team/dion.jpg" alt="Card image cap">
-            <div class="card-body">
-              <h3 class="card-subtitle">Dion</h3>
-              <h5 class="card-title">Project Manager</h5>
-              <p class="card-text">Dion is providing a Project Management role for this project, and assisting back-end roles. </p>
-                <p class="card=text">Dion is a fulltime Technical Services Manager in local government, and completing a Bachelor of Technology for his professional development</p>
+    <div class="row box-black">
+        <div class="col-lg-4">
+          <div class="team dion-bg">
+              <img src="images/team/dion.jpg" class="left" alt="Dion PM">
+              <h3>Dion</h3>
+              <h5>Project Manager</h5>
+              <p>Dion is providing a Project Management role for this project, and assisting back-end roles. </p>
+                <p>Dion is a fulltime Technical Services Manager in local government, and completing a Bachelor of Technology for his professional development</p>
+            </div>
+          </div>
+
+        <div class="col-lg-4">
+        <div class="team tania-bg">
+          <img src="images/team/tania.jpg" class="left" alt="Tania UX Design">
+            <div>
+              <h3>Tania</h3>
+              <h5>UX Design</h5>
+              <p>Tania is new to the design development process, completing a Bachelor of Arts Digital Design.</p>
+                <p></p>
             </div>
           </div>
         </div>
-        <div class="col-lg-4 text-center">
-          <div class="card bg-dark text-white" style="width: 300px;">
-            <img class="card-img-top" src="images/team/tania.jpg" alt="Card image cap">
-            <div class="card-body">
-              <h3 class="card-subtitle">Tania</h3>
-              <h5 class="card-title">UX Design</h5>
-              <p class="card-text">Tania is new to the design development process, completing a Bachelor of Arts Digital Design.</p>
-                <p class="card=text"></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 text-center">
-          <div class="card bg-dark text-white" style="width: 300px;">
-            <img class="card-img-top" src="images/team/barb.jpg" alt="Card image cap">
-            <div class="card-body">
-              <h3 class="card-subtitle">Barb</h3>
-              <h5 class="card-title">Front-end Developer</h5>
-              <p class="card-text">Barbara an experienced front-end developer that is completing a Bachelor of Arts in Internet Communication and Digital Design.</p>
-                <p class="card=text">She is a freelance web developer that maintains small business websites and is creating interactive children’s books</p>
-            </div>
+        <div class="col-lg-4">
+        <div class="team barb-bg">
+            <img src="images/team/barb.jpg" class="left" alt="Barb Front End">
+            <div>
+              <h3>Barb</h3>
+              <h5>Front-end Developer</h5>
+              <p>Barbara an experienced front-end developer that is completing a Bachelor of Arts in Internet Communication and Digital Design.</p>
+
           </div>
         </div>
       </div>
-      <div class="row box-blue text-center">
+    </div>
+      <div class="row box-black">
 
-        <div class="col-lg-6 text-center">
-          <div class="card bg-dark text-white" style="width: 300px;">
-            <img class="card-img-top" src="images/team/jared.jpg" alt="Card image cap">
-            <div class="card-body">
-              <h3 class="card-subtitle">Jared</h3>
-              <h5 class="card-title">Back-end Developer</h5>
-              <p class="card-text">Jared is the back-end developer for this project. He is assisting with the Information Architect role. </p>
-                <p class="card=text">He is completing his Bachelor of IT, and this is his final unit. He is working fulltime as a Workforce Planner.</p>
+        <div class="col-lg-6">
+        <div class="team jared-bg">
+            <img src="images/team/jared.jpg" class="left" alt="Jared Back End">
+            <div>
+              <h3>Jared</h3>
+              <h5>Back-end Developer</h5>
+              <p>Jared is the back-end developer for this project. He is assisting with the Information Architect role. </p>
+                <p>He is completing his Bachelor of IT, and this is his final unit. He is working fulltime as a Workforce Planner.</p>
             </div>
           </div>
         </div>
 
-        <div class="col-lg-6 text-center">
-          <div class="card bg-dark text-white text-center" style="width: 300px;">
-            <img class="card-img-top" src="images/team/michael.jpg" alt="Card image cap">
-            <div class="card-body">
-              <h3 class="card-subtitle">Michael</h3>
-              <h5 class="card-title">Information Architect</h5>
-              <p class="card-text">Michael is the Information Architect for this project. He is also supporting with UX and front-end development. </p>
-                <p class="card=text">Michael is a Bachelor of IT student, studying a minor in internet design and has secured employment as a graduate software integration engineer.</p>
+        <div class="col-lg-6">
+        <div class="team michael-bg">
+            <img src="images/team/michael.jpg" class="left" alt="Michael TA">
+            <div>
+              <h3>Michael</h3>
+              <h5>Information Architect</h5>
+              <p>Michael is the Information Architect for this project. He is also supporting with UX and front-end development. </p>
+                <p>Michael is a Bachelor of IT student, studying a minor in internet design and has secured employment as a graduate software integration engineer.</p>
             </div>
           </div>
         </div>
