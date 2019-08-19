9790e89781c6603fd63c5e4f1c95b6da7ffc5c66
jaredrkelly <jarredkelly@gmail.com>
2019-08-19 15:08:37 +1000

    added messages when trying to log in as stockist and player

--- wholesale.php
+++ wholesale.php
@@ -21,57 +21,62 @@
         //success code
         echo '<p>You are logged in!</p>';
     }*/
-    if(isset($_GET['inuse'])) {
-      echo "<div class='error'>Email address already in use. Try another address or <a href='wholesale.php'>log in.</a></div>";
+    if(isset($_COOKIE["user_verified"])) {
+      echo "<div class='error'>Wholesale purchasing is only available to Desperado stockists.<br>Please see your nearest stockist to purchase or log out of pinata bash to continue.</a></div>";
     }
-    if(isset($_GET['incorrect'])) {
-      echo "<div class='error'>Incorrect login details. Please try again.</div>";
+    else {
+      if(isset($_GET['inuse'])) {
+        echo "<div class='error'>Email address already in use. Try another address or <a href='wholesale.php'>log in.</a></div>";
+      }
+      if(isset($_GET['incorrect'])) {
+        echo "<div class='error'>Incorrect login details. Please try again.</div>";
+      }
+      if(isset($_GET['signup']))
+      {
+          // else show signup form
+          echo '<form action="api/stockist_signup.php" class="sign-up-form text-left" method="post">
+        <div class="form-group">
+          <label for="name">Business Name:</label>
+          <input type="text" class="form-control" name="name" id="name" required><br>
+        </div>
+
+        <div class="form-group">
+          <label for="address">Business Address:</label>
+          <input type="text" class="form-control" name="address" id="address" required><br>
+        </div>
+
+        <div class="form-group">
+          <label for="email">Contact Email:</label>
+          <input type="email" class="form-control" name="email" id="email" required><br>
+        </div>
+
+        <div class="form-group">
+        <label for="pw">Password:</label>
+          <input type="password" class="form-control" name="pw" id="pw" required><br>
+        </div>
+
+        <input type="submit" class="btn btn-dark" value="Signup">
+        <p>Already signed up? <a class="btn btn-warning" href="wholesale.php">Log in!</a></p>
+        </form>';
+      }
+      else
+      {
+          // default - show login form
+        echo '<form action="api/stockist_login.php" class="sign-up-form text-left" method="post">
+        <div class="form-group">
+          <label for="login">Email:</label>
+          <input type="text" class="form-control" name="login" id="login" required><br>
+        </div>
+
+        <div class="form-group">
+        <label for="pw">Password:</label>
+          <input type="password" class="form-control" name="pw" id="pw" required><br>
+        </div>
+
+        <input class="btn btn-dark" type="submit" value="Sign in">
+        <p>New here? <a class="btn btn-warning" href="wholesale.php?signup">Sign up!</a></p>
+      </form>';}
     }
-    if(isset($_GET['signup']))
-    {
-        // else show signup form
-        echo '<form action="api/stockist_signup.php" class="sign-up-form text-left" method="post">
-      <div class="form-group">
-        <label for="name">Business Name:</label>
-        <input type="text" class="form-control" name="name" id="name" required><br>
-      </div>
-
-      <div class="form-group">
-        <label for="address">Business Address:</label>
-        <input type="text" class="form-control" name="address" id="address" required><br>
-      </div>
-
-      <div class="form-group">
-        <label for="email">Contact Email:</label>
-        <input type="email" class="form-control" name="email" id="email" required><br>
-      </div>
-
-      <div class="form-group">
-      <label for="pw">Password:</label>
-        <input type="password" class="form-control" name="pw" id="pw" required><br>
-      </div>
-
-      <input type="submit" class="btn btn-dark" value="Signup">
-      <p>Already signed up? <a class="btn btn-warning" href="wholesale.php">Log in!</a></p>
-      </form>';
-    }
-    else
-    {
-        // default - show login form
-      echo '<form action="api/stockist_login.php" class="sign-up-form text-left" method="post">
-      <div class="form-group">
-        <label for="login">Email:</label>
-        <input type="text" class="form-control" name="login" id="login" required><br>
-      </div>
-
-      <div class="form-group">
-      <label for="pw">Password:</label>
-        <input type="password" class="form-control" name="pw" id="pw" required><br>
-      </div>
-
-      <input class="btn btn-dark" type="submit" value="Sign in">
-      <p>New here? <a class="btn btn-warning" href="wholesale.php?signup">Sign up!</a></p>
-    </form>';}
 ?>
 
 
