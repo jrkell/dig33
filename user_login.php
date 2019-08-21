ba1d4dbff3c80d74e02b0e1a772436e822a838aa
nismotaur <31636049+nismotaur@users.noreply.github.com>
2019-08-21 12:33:48 +1000

    styling and alignment

--- user_login.php
+++ user_login.php
@@ -3,12 +3,18 @@
 
 <h3 class="text-center">Login</h3>
 <form action='api/user_login.php' method='post'>
-    Email:<br>
-    <input type='email' name='email' required>
-    <br>
-    Password:<br>
-    <input type='password' name='password' required><br>
-    <input class="btn btn-dark my-4" type='submit' value='Login'>
+    <div class='form-group'>
+        <label for='email'>Email address</label>
+        <input type='email' name='email' required>
+    </div>
+    <div class='form-group'>
+        <label for='password'>Password</label>
+        <input type='password' name='password' required><br>
+    </div>
+    <div class='form-group'>
+        <input class="btn btn-dark my-4" type='submit' value='Login'>
+
+        <p>Don't have an account?</p>
+        <button class="btn btn-warning my-4" onclick='changeToSignUp()'>Sign Up</button>
+    </div>
 </form>
-<p>Don't have an account?</p>
-<button class="btn btn-warning my-4" onclick='changeToSignUp()'>Sign Up</button>
